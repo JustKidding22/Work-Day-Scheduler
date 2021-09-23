054eaaf9a27899a7079b6f7d4a78bec39598fb9c
James K <projdk90@gmail.com>
2021-09-23 00:05:22 -0700

    initial files

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+// An array of all slots with their id, hour-mark and data.
+//Initially the data field is empty. It will be populated when the user adds notes or reminders.
+var slots = [
+  {
+    id: "0",
+    time: "09",
+    data: "",
+  },
+  {
+    id: "1",
+    time: "10",
+    data: "",
+  },
+  {
+    id: "2",
+    time: "11",
+    data: "",
+  },
+  {
+    id: "3",
+    time: "12",
+    data: "",
+  },
+  {
+    id: "4",
+    time: "13",
+    data: "",
+  },
+  {
+    id: "5",
+    time: "14",
+    data: "",
+  },
+  {
+    id: "6",
+    time: "15",
+    data: "",
+  },
+  {
+    id: "7",
+    time: "16",
+    data: "",
+  },
+  {
+    id: "8",
+    time: "17",
+    data: "",
+  },
+];
